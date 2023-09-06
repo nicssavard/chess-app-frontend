41de5a07b05f7e2567c5c4b632b5670be934175a
nicssavard <105433845+nicssavard@users.noreply.github.com>
2023-09-07 04:33:46 +0800

    new header picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,7 @@ import useStore from "@/store/userStore";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
+import Image from "next/image";
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -96,26 +97,15 @@ export default function Header() {
                 <NavigationMenu.Link asChild>
                   <a
                     className="focus:shadow-violet7 from-purple9 to-indigo9 flex
-                        h-full w-full select-none flex-col justify-end rounded-[6px] bg-gradient-to-b p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px]"
+                        h-full w-full select-none flex-col justify-end rounded-[6px] bg-gradient-to-b  no-underline outline-none focus:shadow-[0_0_0_2px]"
                     href="/"
                   >
-                    <svg
-                      aria-hidden
-                      width="38"
-                      height="38"
-                      viewBox="0 0 25 25"
-                      fill="white"
-                    >
-                      <path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
-                      <path d="M12 0H4V8H12V0Z"></path>
-                      <path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
-                    </svg>
-                    <div className="mt-4 mb-[7px] text-[18px] font-medium leading-[1.2] text-white">
-                      Radix Primitives
-                    </div>
-                    <p className="text-mauve4 text-[14px] leading-[1.3]">
-                      Unstyled, accessible components for React.
-                    </p>
+                    <Image
+                      className="object-cover h-full w-full rounded-md"
+                      src="/noble_chess.png"
+                      width={256}
+                      height={256}
+                    />
                   </a>
                 </NavigationMenu.Link>
               </li>
