62eb0b585267ed12c7f56577368ce5a2bdc127c1
nicssavard <105433845+nicssavard@users.noreply.github.com>
2023-09-12 11:12:09 +0800

    chess game initiated properly

--- src/app/chess/components/Game.tsx
+++ src/app/chess/components/Game.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import { DndContext, DragEndEvent, UniqueIdentifier } from "@dnd-kit/core";
 import Board from "@/app/test/components/Board";
 import { ChessPosition } from "../../../../typings";
+import useStore from "@/store/userStore";
+
 const idToLocation = (id: UniqueIdentifier): ChessPosition => {
   // get coordinates for the board
   id = id.toString();
@@ -39,21 +41,25 @@ interface ChessGame {
   gameType: "random" | "bot" | "friend";
 }
 export default function Game({ gameId, gameType }: ChessGame) {
+  const { user } = useStore((state) => ({ user: state.user }));
   const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
   const [board, setBoard] = useState<string[][] | null>(null);
-  const [turn, setTurn] = useState<"white" | "black">("white");
+  const [userColor, setUserColor] = useState<"w" | "b" | null>(null); //board[1]![0] is the white pawn   board[6]![0] is the black pawn
+  const [turn, setTurn] = useState<"w" | "b">("w");
   const [isCheck, setIsCheck] = useState<boolean>(false);
   const [isCheckMate, setIsCheckMate] = useState<boolean>(false);
   const [win, setWin] = useState<"white" | "black" | "none">("none");
-
+  console.log(userColor);
   useEffect(() => {
     // Initialize WebSocket connection
-    if (gameId) {
+    if (gameId && user?.id) {
       var ws = new WebSocket(
-        `ws://127.0.0.1:8000/chessGame?chessGameId=${gameId}`
+        `ws://127.0.0.1:8000/chessGame?chessGameId=${gameId}&userId=${user?.id}`
       );
     } else {
-      var ws = new WebSocket(`ws://127.0.0.1:8000/chessGame?chessGameId=${""}`);
+      var ws = new WebSocket(
+        `ws://127.0.0.1:8000/chessGame?chessGameId=${""}&userId=${user?.id}`
+      );
     }
     setWebSocket(ws);
 
@@ -72,15 +78,22 @@ export default function Game({ gameId, gameType }: ChessGame) {
         // console.log(`Message received: ${e.data}`);
         const newMessage = JSON.parse(e.data);
         console.log(newMessage);
-        // const newBoard = newMessage.fen
-        //   .split(" ")[0]
-        //   .split("/")
-        //   .reverse()
-        //   .join("/");
-        const newBoard = fenToBoard(
-          newMessage.fen.split(" ")[0].split("/").reverse().join("/")
-        );
-
+        let newBoard;
+        if (newMessage.color) {
+          newBoard = fenToBoard(
+            newMessage.board.fen.split(" ")[0].split("/").reverse().join("/")
+          );
+          setUserColor(newMessage.color);
+        } else {
+          // const newBoard = newMessage.fen
+          //   .split(" ")[0]
+          //   .split("/")
+          //   .reverse()
+          //   .join("/");
+          newBoard = fenToBoard(
+            newMessage.fen.split(" ")[0].split("/").reverse().join("/")
+          );
+        }
         console.log(newBoard);
         // setChat((chat) => {
         //   if (!chat) return null;
@@ -140,7 +153,7 @@ export default function Game({ gameId, gameType }: ChessGame) {
             <Board
               board={board}
               className={`rounded-2xl relative overflow-hidden border-4 ${
-                turn === "white" ? "border-white" : "border-black"
+                turn === "w" ? "border-white" : "border-black"
               }`}
             />
           )}
