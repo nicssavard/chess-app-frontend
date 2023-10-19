cd78d318db5a7c316a9f9905dc948887b203168a
nicssavard <nicssavard@gmail.com>
2023-10-19 04:25:57 -0400

    basic game searching xomponent

--- src/app/chess/components/Waiting.tsx
+++ src/app/chess/components/Waiting.tsx
@@ -1,5 +1,14 @@
+import { LoadingSpinner } from "@/components/ui/Loading";
+
 const Waiting = () => {
-  return <div>Waiting</div>;
+  return (
+    <div className=" rounded-lg bg-gray-900 p-6">
+      <h1 className="text-2xl">Searching for an opponent</h1>
+      <div className="flex justify-center">
+        <LoadingSpinner size={60} />
+      </div>
+    </div>
+  );
 };
 
 export default Waiting;
